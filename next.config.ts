ab8d01fab6dc78c31dbadd31bbeaa2e0d25860a4
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2025-09-22 17:26:17 +0530

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    allowedDevOrigins: [
+      "local-origin.dev",
+      "*.local-origin.dev",
+      "http://192.168.1.8:3000/_next/*",
+    ],
 };
 
 export default nextConfig;
