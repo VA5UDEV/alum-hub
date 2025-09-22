d4414e6dfd9417753ab5e87c0328a15931a46e30
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2025-09-22 17:25:59 +0530

    feat: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,7 @@
-import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
+  out: "./drizzle/migrations",
   schema: "./db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
